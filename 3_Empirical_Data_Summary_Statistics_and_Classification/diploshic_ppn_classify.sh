cdbc16de93f4bf053e613e7fd34b005e9c02cd4e
Colin M. Brand <44248136+brandcm@users.noreply.github.com>
2020-12-15 13:18:33 -0800

    Update diploshic_ppn_classify.sh

--- 3_Empirical_Data_Summary_Statistics_and_Classification/diploshic_ppn_classify.sh
+++ 3_Empirical_Data_Summary_Statistics_and_Classification/diploshic_ppn_classify.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Indicate resource specification(s) (e.g., partition, job type, output, error) here or with SBATCH command
+
+# Load packages
+module load python3
+module load diploshic
+
+# Code
 for f in ~/ppn_chr*.fvec; do diploSHIC.py predict ppModel.json ppModel.weights.hdf5 $f $f.preds; done 
